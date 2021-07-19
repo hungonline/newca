1ecd6d8a3a178b7ceab4779f4aabf0d89793e6bf
hungonline <hung91bn@gmail.com>
2021-07-19 15:42:27 +0700

    fix
    fix

--- HTML-PC/js/common.js
+++ HTML-PC/js/common.js
@@ -154,6 +154,7 @@
         );
     }
 
+
     //slide Gallery
     function swiper() {
         var swiperCustomer = new Swiper(".box-customer .swiper-container", {
@@ -271,7 +272,44 @@
                 prevEl: ".swiper-rating-prev",
             }
         });
+    }
 
+    //only scroll
+    function onlyscroll() {
+        var lastId,
+        topMenu = $(".tab-detail"),
+        topMenuHeight = topMenu.outerHeight() + 69,
+        menuItems = topMenu.find("a"),
+        scrollItems = menuItems.map(function () {
+            var item = $($(this).attr("href"));
+            if (item.length) {
+                return item;
+            }
+        });
+        menuItems.click(function (e) {
+            var href = $(this).attr("href"),
+                offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+            $('html, body').stop().animate({
+                scrollTop: offsetTop
+            }, 300);
+            e.preventDefault();
+        });
+    
+        // Bind to scroll
+        $(window).scroll(function () {
+            var fromTop = $(this).scrollTop() + topMenuHeight;
+            var cur = scrollItems.map(function () {
+                if ($(this).offset().top < fromTop) return this;
+            });
+            cur = cur[cur.length - 1];
+            var id = cur && cur.length ? cur[0].id :
+                "";
+            if (lastId !== id) {
+                lastId = id;
+                menuItems.parent().removeClass("active")
+                    .end().filter("[href='#" + id + "' ]").parent().addClass("active");
+            }
+        });
     }
 
     $(function () {
@@ -280,6 +318,8 @@
         onCLick();
         hover();
         swiper();
+        onlyscroll();
         $('[data-toggle="tooltip"]').tooltip();
+        
     });
 })(jQuery);
